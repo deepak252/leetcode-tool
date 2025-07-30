5834484b4042fc8123aa044210dc5703ced73248
deepak252 <deepakkudc16102000@gmail.com>
2025-07-31 00:14:59 +0530

    Extension init

---
+++ lc-extension/eslint.config.js
@@ -0,0 +1,65 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+export default tseslint.config(
+  { ignores: ['dist', 'build', 'node_modules'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+  }
+)
+
+// import js from '@eslint/js'
+// import globals from 'globals'
+// import reactHooks from 'eslint-plugin-react-hooks'
+// import reactRefresh from 'eslint-plugin-react-refresh'
+// import tseslint from 'typescript-eslint'
+// import { globalIgnores } from 'eslint/config'
+
+// export default tseslint.config([
+//   globalIgnores(['dist']),
+//   {
+//     files: ['**/*.{ts,tsx}'],
+//     extends: [
+//       js.configs.recommended,
+//       tseslint.configs.recommended,
+//       reactHooks.configs['recommended-latest'],
+//       reactRefresh.configs.vite,
+//     ],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//     },
+//   },
+// ])
