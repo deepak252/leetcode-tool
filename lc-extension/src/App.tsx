cdf2d011a7bcb543d67d80cd883fcdeaac243cc5
deepak252 <deepakkudc16102000@gmail.com>
2025-08-01 01:24:19 +0530

    lc-extension: Absolute import and svg icons setup

--- lc-extension/src/App.tsx
+++ lc-extension/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Logo from '@/assets/react.svg?react'
 import './App.css'
 
 function App() {
@@ -9,11 +8,13 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <Logo
+            width={100}
+            className="logo react"
+            height={40}
+            fill="blue"
+          />{' '}
         </a>
       </div>
       <h1 className="bg-red-400">Vite + React</h1>
