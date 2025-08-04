6d636b852574687728aab45ea507191770de6d9e
deepak252 <deepakkudc16102000@gmail.com>
2025-08-05 01:10:51 +0530

    tut-extension: react+vite+ts

--- lc-extension/src/App.tsx
+++ lc-extension/src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
 import Logo from '@/assets/react.svg?react'
-import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState<any>()
+
+  const onClick = async () => {
+    // Popup Context
+    console.log('Popup context')
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    })
+    chrome.scripting.executeScript<string[], void>({
+      target: { tabId: tab.id! },
+      args: [color],
+      func: (color) => {
+        console.log('Page context')
+        // This runs in the web page (not the popup)
+        // document.body.innerHTML = "";
+        // document.body.style.backgroundColor = 'red'
+        document.body.style.backgroundColor = color
+        // alert('Hello from my extension')
+      },
+    })
+  }
+
+  // const onClick = async () => {
+  //   // Popup Context
+  //   console.log('Page context')
+  //   const [tab] = await chrome.tabs.query({
+  //     active: true,
+  //     currentWindow: true,
+  //   })
+  //   chrome.scripting.executeScript({
+  //     target: { tabId: tab.id! },
+  //     func: () => {
+  //       console.log('Page context')
+  //       // This runs in the web page (not the popup)
+  //       // document.body.innerHTML = "";
+  //       // document.body.style.backgroundColor = 'red'
+  //       alert('Hello from my extension')
+  //     },
+  //   })
+  // }
 
   return (
     <>
       <div>
         <a href="https://react.dev" target="_blank">
-          <Logo
-            width={100}
-            className="logo react"
-            height={40}
-            fill="blue"
-          />{' '}
+          <Logo width={100} className="logo react" height={40} fill="blue" />
         </a>
       </div>
-      <h1 className="bg-red-400">Vite + React</h1>
+      <h1 className="bg-red-400">My Vite Extension</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <input type="color" onChange={(e) => setColor(e.currentTarget.value)} />
+        <button onClick={onClick}>Click Me</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
